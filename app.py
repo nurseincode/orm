50907be3991c2a6d771f9d587c5388c64eae9860
https://github.com/nurseincode <nurseincode@gmail.com>
2026-01-05 22:19:04 +1100

    Completed declaring Model

--- app.py
+++ app.py
@@ -11,8 +11,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://orm_dev:123456@lo
 db = SQLAlchemy(app)
 
 # Model
+# Just declares and configures the model in memory - the physical DB is unaffected
 class Product(db.Model):
     __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Float(precision=2))
+    stock = db.Column(db.Integer, db.CheckConstraint('stock>=0'))
+
+
 
 
 @app.route('/')
