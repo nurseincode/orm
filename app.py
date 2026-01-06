fd406495917d718ff82620f859d40543566bf250
https://github.com/nurseincode <nurseincode@gmail.com>
2026-01-06 18:42:23 +1100

    Create table products completed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy import Numeric
 
 
 app = Flask(__name__)
@@ -19,12 +20,20 @@ class Product(db.Model):
 
     name = db.Column(db.String(150), nullable=False)
     description = db.Column(db.Text)
-    price = db.Column(db.Float(precision=2))
+    price = db.Column(db.Numeric(10,2))
     stock = db.Column(db.Integer, db.CheckConstraint('stock>=0'))
-
-
-
 
 @app.route('/')
 def home():
     return 'Hello!'
+
+
+@app.route('/init_db')
+def init_db():
+    db.create_all()
+    return('Created tables')
+
+
+
+
+
